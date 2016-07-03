024ed789143956c798234489e736dd6b4467d273
athomsfere <athomsfere@gmail.com>
2016-07-03 11:23:31 -0500

    Initialize

---
+++ Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtomFormat
+{
+    namespace Numbers
+    {        
+            public static class Format
+            {
+                /// <summary>
+                /// Easily convert int to its binary string representation
+                /// </summary>
+                /// <param name="StandardFormatInt"></param>
+                /// <returns></returns>
+                public static string ToBinary(this int StandardFormatInt)
+                {
+                    return Convert.ToString(StandardFormatInt, 2);
+                }
+            }
+        
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AtomFormat;
+
+namespace AtomFormat
+{
+    //namespace Numbers
+    //{
+    //    public static class Format
+    //    {
+    //        public static string ToBinary(this int StandardFormatInt)
+    //        {
+    //            return Convert.ToString(StandardFormatInt, 2);
+    //        }
+    //    }
+    //}
+
+    namespace Time
+    {
+        public static class TimeMaps
+        {
+
+        }
+        public static class Time
+        {
+            internal static Dictionary<TimeOfDay, string> timeMap = new Dictionary<TimeOfDay, string>()
+            {
+                {TimeOfDay.BeginningOf12Day, "yyyy/MM/dd 12:00:00 tt"},
+                {TimeOfDay.Noon, "yyyy/MM/dd 12:00:00 PM"},
+                {TimeOfDay.EndOf12Day, "yyyy/MM/dd 11:59:59 tt"},
+                {TimeOfDay.EndOf24Day, "yyyy/MM/dd 23:59:59"}
+            };
+
+                public enum TimeOfDay
+                {
+                    BeginningOf12Day,
+                    BeginningOf24Day,
+                    EndOf12Day,
+                    EndOf24Day,
+                    Noon
+                }
+
+                public static string TimeFormat(this DateTime timeOfDay, TimeOfDay Format)
+                {
+                    timeOfDay.TimeManipulation(Format);
+                    return timeOfDay.ToString(timeMap[Format].ToString());
+                }
+
+                private static void TimeManipulation(this DateTime TimeToCheck, TimeOfDay timeOfDay)
+                {
+                    switch (timeOfDay)
+                    {
+                        case TimeOfDay.BeginningOf12Day:
+                        case TimeOfDay.BeginningOf24Day:
+                            TimeToCheck.TimeOfDay.Subtract(TimeSinceMidnight(TimeToCheck));
+                            break;
+
+                    };
+                }
+
+                private static TimeSpan TimeSinceMidnight(DateTime CurrentTime)
+                {
+                    TimeSpan HowLongAgo = CurrentTime - CurrentTime.Subtract(CurrentTime.TimeOfDay);
+                    return HowLongAgo + TimeSpan.FromMinutes(1);
+                }
+        }
+    }
+
+    public enum Countries
+    {
+        USA,
+        FRANCE,
+        BRITAIN,
+        JAPAN
+    }
+
+    
+    namespace Currency
+    {
+        
+        public static class Formatting
+        {
+            internal static Dictionary<Countries, string> currencyMap = new Dictionary<Countries, string>()
+            {
+                {Countries.USA, "us-US"},
+                {Countries.FRANCE, "fr-FR"},
+                {Countries.BRITAIN, "br, EN"},
+                {Countries.JAPAN, "ja-JP"}
+            };
+
+            public static string ToCurrencyOf<T>(this T CurrencyAmount, Countries Country) where T : new()
+            {
+                decimal currency = new decimal();
+                System.Globalization.CultureInfo newCulture = new System.Globalization.CultureInfo(currencyMap[Country]);
+
+                var currencyString = String.Format(newCulture, "{0:C}", CurrencyAmount);
+                return currencyString;         
+            }
+        }
+    }
+}
