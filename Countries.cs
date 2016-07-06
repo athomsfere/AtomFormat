4bb2bbb533cfa1a762670b5a0ecaa5ea864a083b
athomsfere <athomsfere@gmail.com>
2016-07-05 21:16:10 -0500

    Add methods, reset GIT.

---
+++ Countries.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtomFormat
+{
+    public enum Countries
+    {
+        AUSTRALIA,
+        BRITAIN,
+        CANADA,
+        CHINA,
+        FRANCE,
+        FRENCH_CANADIAN,
+        
+        JAPAN,
+        RUSSIA,
+        SINGAPORE,
+        SINGAPORE_SIMPLIFIED,
+        USA,
+
+    }
+    public static class Mapping
+    {
+        internal static Dictionary<Countries, string> currencyMap = new Dictionary<Countries, string>()
+        {
+            {Countries.AUSTRALIA, "de-AT"},
+            {Countries.BRITAIN, "br, EN"},
+            {Countries.CANADA, "en-CA"},
+            {Countries.CHINA, "zh-CN"},
+            
+            {Countries.FRANCE, "fr-FR"},
+            {Countries.FRENCH_CANADIAN, "fr-CA"},
+            {Countries.RUSSIA, "ru-RU"},
+
+            {Countries.SINGAPORE, "en-SG"},
+            {Countries.SINGAPORE_SIMPLIFIED, "zh-SG"},
+
+            {Countries.JAPAN, "ja-JP"},
+            {Countries.USA, "us-US"}
+        };
+    }
+}
---
+++ Numbers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtomFormat
+{
+    namespace Numbers
+    {        
+            public static class Format
+            {
+                /// <summary>
+                /// Easily convert int to its binary string representation
+                /// </summary>
+                /// <param name="StandardFormatInt"></param>
+                /// <returns></returns>
+                public static string ToBinary(this int StandardFormatInt)
+                {
+                    return Convert.ToString(StandardFormatInt, 2);
+                }
+
+                #region ToPercentage
+                public static decimal ToPercentageDecimal<T>(this T Value) where T : new()
+                {
+                    decimal percentage = Convert.ToDecimal(Value);
+                    percentage = (percentage / 100);
+                    return percentage;
+                }
+
+                public static string ToPercentageDecimal(this string Value)
+                {
+                    decimal percentage = Convert.ToDecimal(Value);
+                    return percentage.ToPercentageDecimal().ToString();
+                }
+
+                public static string ToPercentageDecimalString<T>(this T Value) where T : new()
+                {                    
+                    var percentage = Value.ToPercentageDecimal();
+
+                    return percentage.ToString();
+                }
+
+                public static string ToPercentageDecimalString(this string Value)
+                {
+                    decimal percentage = decimal.Parse(Value);
+                    percentage.ToPercentageDecimal();
+
+                    return percentage.ToString();
+                }
+                #endregion
+
+                #region PercentageString
+                public static string ToPercentageString<T>(this T DecimalPercentage) where T: new()
+                {
+                    var percentNumber = Convert.ToDecimal(DecimalPercentage);
+                    return percentNumber.ToPercentageDecimal() + "%";
+                }
+
+                public static string ToPercentageString<T>(this T DecimalPercentage, int DecimalPlaceAccuracy) where T : new()
+                {
+                    var percentNumber = Convert.ToDecimal(DecimalPercentage);
+                    percentNumber = percentNumber.ToPercentageDecimal() / 1.000000000000000000000000000000000m;
+                    string percentString = percentNumber.ToString();
+
+                    if (!percentString.Contains(".") & DecimalPlaceAccuracy > 0)
+                    {
+                        percentString += ".";
+                    }
+
+                    if (DecimalPlaceAccuracy <= 0)
+                    {
+                        return percentString;
+                    }
+
+                    int zeroesToAdd = percentString.Split('.')[1].Length;
+
+                    for (int i = DecimalPlaceAccuracy; i > 0; i--)
+                    {
+                        percentString += "0";
+                    }
+                    return percentString + "%";
+                }
+
+                public static decimal ToPercentageDecimal(this decimal DecimalPercentage)
+                {
+                    var percentageNumber = DecimalPercentage * 100;
+                    return percentageNumber;
+                }
+                #endregion
+            }
+        
+    }
+}
